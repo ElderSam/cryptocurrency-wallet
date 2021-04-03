60f7606e64884460091e82d65068480029b8c391
ElderSam <eldersamuel98@gmail.com>
2021-04-02 22:03:32 -0300

    SignUp component

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,27 +1,30 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 import './SignUp.css';
 
-export default class SignUp extends Component {
-    render() {
-        return(
+export default function SignUp() {
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
+
+    return(
+        <div>
+            <h2>Cadastro de Usuário</h2>
+            <form onSubmit={() => alert('submited')}>
+
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" ref={emailRef} required />
+
+                <label htmlFor="password">Senha</label>
+                <input type="password" id="password" ref={passwordRef} required />
+
+                <label htmlFor="password-confirm">Repita a Senha</label>
+                <input type="password" id="password-confirm" ref={passwordConfirmRef} required />
+
+                <input type="submit" value="Cadastrar"/>
+            </form>
             <div>
-                <h2>Cadastro de Usuário</h2>
-                <form onSubmit={() => alert('submited')}>
-                    <label htmlFor="user">Usuário</label>
-                    <input type="text" name="user" id="user" required />
-
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" required />
-
-                    <label htmlFor="password">Senha</label>
-                    <input type="password" name="password" id="password" required />
-
-                    <label htmlFor="password2">Repita a Senha</label>
-                    <input type="password" name="password2" id="password2" required />
-
-                    <input type="submit" value="Cadastrar"/>
-                </form>
+                Already have an account? Log In
             </div>
-        )
-    }
+        </div>
+    )
 }
