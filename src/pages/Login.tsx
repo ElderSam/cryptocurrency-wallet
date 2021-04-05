9a495f8c53f91c462a8fc381aba442479896b651
ElderSam <eldersamuel98@gmail.com>
2021-04-05 10:17:34 -0300

    add Material-UI to LogIn

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,43 @@
 import React, { useRef, useState } from 'react'
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 import { useAuth } from '../contexts/AuthContext'
 
-import './index.css'
+//import './index.css'
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+}));
 
 export default function Login() {
+    const classes = useStyles();
+
     const emailRef:any = useRef()
     const passwordRef:any = useRef()
 
@@ -31,30 +63,68 @@ export default function Login() {
     }
 
     return(
-        <div>
-            <h2>Log In</h2>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
             {error && <div className="statusMessage">{error}</div>}
 
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" ref={emailRef} required />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Login{/* Sign in */}
+                </Typography>
+                <form onSubmit={handleSubmit} className={classes.form} /*noValidate*/ >
+                    <TextField
+                        ref={emailRef}
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email"
+                        type="email"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        ref={passwordRef}
+                        inputProps={{ minLength: 6 }}
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Senha"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
 
-                    <label htmlFor="password">Senha</label>
-                    <input type="password" id="password" ref={passwordRef} required minLength={6} />
-
-                    <input disabled ={loading} type="submit" value="Entrar"/>
+                    <Button
+                        disabled ={loading}
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Entrar
+                    </Button>
                 </form>
                 <div>
-                    <Link to="/forgot-password">Esqueceu sua senha?</Link>
+                    <Link href="/forgot-password">Esqueceu sua senha?</Link>
+                    <div>
+                        {/* Do you need an account? Log In */}
+                        Precisa de uma conta?&nbsp;
+                        <Link href="/signup">Cadastrar</Link>
+                    </div>
                 </div>
             </div>
-
-            <div>
-                {/* Do you need an account? Log In */}
-                Precisa de uma conta?&nbsp;
-                <Link to="/signup">Cadastrar</Link>
-            </div>
-        </div>
+            <Box mt={8}>
+            {/* <Copyright /> */}
+            </Box>
+        </Container>
     )
 }
