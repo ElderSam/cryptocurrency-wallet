55e85a07471b3333eb29ee9e2c3a4ef9d011b0b6
ElderSam <eldersamuel98@gmail.com>
2021-04-05 15:04:10 -0300

    Create Material-UI Theme for components

---
+++ src/components/Theme.tsx
@@ -0,0 +1,37 @@
+
+import { makeStyles } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const myTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#b286b9',
+    },
+    secondary: {
+      main: '#36f436',
+    },
+  },
+});
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: myTheme.palette.primary.main,
+  },
+}));
+
+export default useStyles;
