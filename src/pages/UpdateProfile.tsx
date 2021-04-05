e3c5a2e3218d6841f71b948bcaccc2b2f5d2091d
ElderSam <eldersamuel98@gmail.com>
2021-04-05 16:19:27 -0300

    add Material-UI to UpdateProfile

--- src/pages/UpdateProfile.tsx
+++ src/pages/UpdateProfile.tsx
@@ -1,11 +1,22 @@
 import React, { useRef, useState } from 'react'
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import { useAuth } from '../contexts/AuthContext'
 
-import './index.css'
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Avatar from '@material-ui/core/Avatar';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+import useStyles from '../components/Theme'
 
 export default function UpdateProfile() {
+    const classes = useStyles();
+
     const emailRef:any = useRef()
     const passwordRef:any = useRef()
     const passwordConfirmRef:any = useRef()
@@ -52,42 +63,73 @@ export default function UpdateProfile() {
 
     return(
         //console.log(JSON.stringify(currentUser))
-        <div>
-            <h2>Atualizar Perfil</h2>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
             {error && <div className="statusMessage">{error}</div>}
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="email">Email</label>
-                <input
-                    type="email"
-                    id="email"
-                    ref={emailRef}
-                    required
-                    defaultValue={currentUser.email}
-                />
 
-                <label htmlFor="password">Senha</label>
-                <input
-                    type="password"
-                    id="password"
-                    ref={passwordRef} 
-                    placeholder="Deixe em branco para não mudar"
-                    minLength={6}
-                /> {/* Leave blank to keep the same */}
-
-                <label htmlFor="password-confirm">Repita a Senha</label>
-                <input
-                    type="password"
-                    id="password-confirm"
-                    ref={passwordConfirmRef}
-                    placeholder="Deixe em branco para não mudar"
-                    minLength={6}
-                /> {/* Leave blank to keep the same */}
-
-                <input disabled ={loading} type="submit" value="Atualizar"/>
-            </form>
-            <div>
-                <Link to="/">Cancelar</Link> 
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <AccountCircleIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Atualizar Perfil
+                </Typography>
+                <form onSubmit={handleSubmit} className={classes.form} /*noValidate*/ >
+                    <TextField
+                        inputRef={emailRef}
+                        defaultValue={currentUser.email}
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email"
+                        type="email"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        inputRef={passwordRef}
+                        placeholder="Deixe em branco para não mudar" // Leave blank to keep the same
+                        inputProps={{ minLength: 6 }}
+                        variant="outlined"
+                        margin="normal"
+                        fullWidth
+                        name="password"
+                        label="Senha"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <TextField
+                        inputRef={passwordConfirmRef}
+                        placeholder="Deixe em branco para não mudar" // Leave blank to keep the same
+                        inputProps={{ minLength: 6 }}
+                        variant="outlined"
+                        margin="normal"
+                        fullWidth
+                        name="password"
+                        label="Confirmação da Senha"
+                        type="password"
+                        id="password-confirm"
+                        autoComplete="current-password"
+                    />
+                    <Button
+                        disabled ={loading}
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        // color="primary"
+                        className={classes.submit}
+                    >
+                        Atualizar
+                    </Button>
+                </form>
+                <div>
+                    <Link href="/">Cancelar</Link> 
+                </div>
             </div>
-        </div>
+        </Container>
     )
 }
